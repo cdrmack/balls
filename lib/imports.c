cd7e192d01697e4d632014eb9d5cd7c147f73892
cdrmack <git.pynvt@passmail.net>
2025-02-05 15:50:09 +0100

    run js from C async

--- lib/imports.c
+++ lib/imports.c
@@ -8,5 +8,8 @@ int main()
     // call JS function
     emscripten_run_script("console.log('js log called from C')");
 
+    // call JS function async
+    emscripten_async_run_script("alert('js alert called from C - async')", 2000); // 2000ms
+
     return 0;
 }
