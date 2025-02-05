276a641807d1492c070d126afe457becbce24e66
cdrmack <git.pynvt@passmail.net>
2025-02-05 15:57:59 +0100

    store return values from js functions in C

--- lib/imports.c
+++ lib/imports.c
@@ -9,7 +9,15 @@ int main()
     emscripten_run_script("console.log('js log called from C')");
 
     // call JS function async
-    emscripten_async_run_script("alert('js alert called from C - async')", 2000); // 2000ms
+    emscripten_async_run_script("alert('js alert called from C - async')",
+                                2000); // 2000ms
+
+    // get return value from the JS function
+    int jsValInt = emscripten_run_script_int("getNum()");
+    printf("Called js getNum(), it returned: %d\n", jsValInt);
+
+    char *jsValStr = emscripten_run_script_string("getStr()");
+    printf("Called js getStr(), it returned: %s\n", jsValStr);
 
     return 0;
 }
