d328f58c2637f924cf5cdfd478ab2a6829a2df59
cdrmack <git.pynvt@passmail.net>
2025-02-07 11:38:35 +0100

    draw random circles generated by C

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 # -O3
 
 all:
-	emcc lib/canvas.c -s WASM=1 -s EXPORTED_FUNCTIONS=_main -s EXPORTED_RUNTIME_METHODS=ccall,cwrap -o public/canvas.js
+	emcc lib/canvas.c -s WASM=1 -s EXPORTED_FUNCTIONS=_main,_get_circles -s EXPORTED_RUNTIME_METHODS=ccall,cwrap -o public/canvas.js
 
 demo:
 	emcc lib/demo.c -s WASM=1 -s EXPORTED_FUNCTIONS=_main,_foo,_hello -s EXPORTED_RUNTIME_METHODS=ccall,cwrap -o public/demo.js
--- lib/canvas.c
+++ lib/canvas.c
@@ -1,9 +1,96 @@
-#include <stdio.h>
 #include <emscripten.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define NUMBER_OF_CIRCLES 40
+
+/*
+typedef struct Position
+{
+    int x;
+    int y;
+
+} Position;
+
+typedef struct Color
+{
+    int red;
+    int green;
+    int blue;
+} Color;
+
+Color random_color()
+{
+    Color tmp = {
+        .red = rand() % 255, .green = rand() % 255, .blue = rand() % 255};
+
+    return tmp;
+}
+
+typedef struct Circle
+{
+    Position position;
+    int radius;
+    Color color;
+
+} Circle;
+
+Circle random_circle()
+{
+    Circle tmp;
+    tmp.radius = rand() % 50;
+    tmp.position.x = rand() % 1000 + tmp.radius;
+    tmp.position.y = rand() % 1000 + tmp.radius;
+    tmp.color = random_color();
+
+    return tmp;
+}
+*/
+
+typedef struct Circle
+{
+    int x;
+    int y;
+    int r;
+    int cr;
+    int cg;
+    int cb;
+
+} Circle;
+
+Circle random_circle()
+{
+    Circle tmp;
+    tmp.r = rand() % 50;
+    tmp.x = rand() % 1000 + tmp.r;
+    tmp.y = rand() % 1000 + tmp.r;
+    tmp.cr = rand() % 255;
+    tmp.cg = rand() % 255;
+    tmp.cb = rand() % 255;
+
+    return tmp;
+}
+
+Circle circles[NUMBER_OF_CIRCLES];
+
+Circle *get_circles() { return circles; }
 
 int main()
 {
     printf("init circles\n");
-    emscripten_run_script("render()");
+
+    srand(time(NULL));
+
+    // create circles
+    for (int i = 0; i < NUMBER_OF_CIRCLES; ++i)
+    {
+        circles[i] = random_circle();
+    }
+
+    /* emscripten_run_script("render()"); */
+    EM_ASM(
+        { render($0, $1); }, NUMBER_OF_CIRCLES * 6,
+        6); // Circle struct consist of 6 ints
     return 0;
 }
