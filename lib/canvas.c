0756dab4ccf0b6a4438148b83fe028d35251a03b
cdrmack <git.pynvt@passmail.net>
2025-02-07 13:36:27 +0100

    animate circles

--- lib/canvas.c
+++ lib/canvas.c
@@ -50,14 +50,25 @@ Circle random_circle()
 
 typedef struct Circle
 {
-    int x;
-    int y;
-    int r;
-    int cr;
-    int cg;
-    int cb;
-
+    int x;  // position x
+    int y;  // position y
+    int r;  // radius
+    int cr; // color r
+    int cg; // color g
+    int cb; // color b
 } Circle;
+
+typedef struct CircleAnimationData
+{
+    int x;  // position x
+    int y;  // position y
+    int r;  // radius
+    int vx; // velocity x
+    int vy; // velocity y
+    int dx; // direction x (1 = forward)
+    int dy; // direction y (1 = forward)
+
+} CircleAnimationData;
 
 Circle random_circle()
 {
@@ -73,8 +84,60 @@ Circle random_circle()
 }
 
 Circle circles[NUMBER_OF_CIRCLES];
+CircleAnimationData animation_data[NUMBER_OF_CIRCLES];
 
-Circle *get_circles() { return circles; }
+Circle *get_circles(int canvas_width, int canvas_height)
+{
+    // update animation
+    for (int i = 0; i < NUMBER_OF_CIRCLES; ++i)
+    {
+        // collision
+        if ((animation_data[i].x + animation_data[i].r) >= canvas_width)
+        {
+            animation_data[i].dx = 0;
+        }
+
+        if ((animation_data[i].x - animation_data[i].r) <= 0)
+        {
+            animation_data[i].dx = 1;
+        }
+
+        if ((animation_data[i].y + animation_data[i].r) >= canvas_height)
+        {
+            animation_data[i].dy = 0;
+        }
+
+        if ((animation_data[i].y - animation_data[i].r) <= 0)
+        {
+            animation_data[i].dy = 1;
+        }
+
+        // update position
+        if (animation_data[i].dx == 1)
+        {
+            animation_data[i].x += animation_data[i].vx;
+        }
+        else
+        {
+            animation_data[i].x -= animation_data[i].vx;
+        }
+
+        if (animation_data[i].dy == 1)
+        {
+            animation_data[i].y += animation_data[i].vy;
+        }
+        else
+        {
+            animation_data[i].y -= animation_data[i].vy;
+        }
+
+        // update circles
+        circles[i].x = animation_data[i].x;
+        circles[i].y = animation_data[i].y;
+    }
+
+    return circles;
+}
 
 int main()
 {
@@ -85,7 +148,29 @@ int main()
     // create circles
     for (int i = 0; i < NUMBER_OF_CIRCLES; ++i)
     {
-        circles[i] = random_circle();
+        int r = rand() % 50;
+        int x = rand() % 1000 + r;
+        int y = rand() % 1000 + r;
+        int cr = rand() % 255;
+        int cg = rand() % 255;
+        int cb = rand() % 255;
+
+        // circle
+        circles[i].x = x;
+        circles[i].y = y;
+        circles[i].r = r;
+        circles[i].cr = cr;
+        circles[i].cg = cg;
+        circles[i].cb = cb;
+
+        // circle animation
+        animation_data[i].x = x;
+        animation_data[i].y = y;
+        animation_data[i].r = r;
+        animation_data[i].vx = rand() % 10;
+        animation_data[i].vy = rand() % 10;
+        animation_data[i].dx = 1;
+        animation_data[i].dy = 1;
     }
 
     /* emscripten_run_script("render()"); */
